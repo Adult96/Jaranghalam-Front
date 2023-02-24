8dbbd0f29795b240a32f285964da2e7b037bc808
Adult96 <djxor007@gmail.com>
2023-02-24 18:09:35 +0900

    feat: date utils 함수 구현 및 board item 컴포넌트에서 사용

--- src/components/BoardItem.jsx
+++ src/components/BoardItem.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import formatAgo from '../utils/date';
 
 export default function BoardItem({
   board: { nickName, title, content, imageUrl, createdAt },
 }) {
+  const setDate = date => {
+    return formatAgo(date);
+  };
+
   return (
     <BoardContainer>
       <div>
         <span>{nickName}</span>
-        <span>{createdAt}</span>
+        <span>{setDate(createdAt)}</span>
       </div>
-      <img src={imageUrl} alt='userimg' />
-      <h1>{title}</h1>
+      <Img src={imageUrl} alt='userimg' />
+      <AiOutlineHeart />
+      <AiFillHeart />
+      <h3>{title}</h3>
       <p>{content}</p>
     </BoardContainer>
   );
@@ -19,4 +27,14 @@ export default function BoardItem({
 
 const BoardContainer = styled.div`
   width: 100%;
+  height: 100%;
+  :hover {
+    opacity: 0.5;
+  }
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 75%;
+  object-fit: cover;
 `;
