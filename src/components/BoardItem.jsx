f27ac3f01a11d14725a6af5e9f4fd34b3dca3587
Adult96 <djxor007@gmail.com>
2023-02-24 23:57:01 +0900

    design: board item text break style 설정

--- src/components/BoardItem.jsx
+++ src/components/BoardItem.jsx
@@ -12,16 +12,16 @@ export default function BoardItem({
 
   return (
     <BoardContainer>
-      <Title>
+      <Header>
         <h3>{nickName}</h3>
         <Date>{setDate(createdAt)}</Date>
-      </Title>
+      </Header>
       <Img src={imageUrl} alt='userimg' />
       <AiOutlineHeart />
       <AiFillHeart />
       <p>{`좋아요 ${10}개`}</p>
-      <h4>{`${nickName} ${title}`}</h4>
-      <p>{content}</p>
+      <Title>{`${nickName} ${title}`}</Title>
+      <Content>{content}</Content>
     </BoardContainer>
   );
 }
@@ -37,7 +37,7 @@ const BoardContainer = styled.div`
   }
 `;
 
-const Title = styled.div`
+const Header = styled.div`
   display: flex;
   align-items: center;
   gap: 0.3rem;
@@ -53,4 +53,23 @@ const Img = styled.img`
   height: 60%;
   border-radius: 0.5rem;
   object-fit: cover;
+`;
+
+const Title = styled.h4`
+  display: -webkit-box;
+
+  height: 1.08rem;
+  white-space: normal;
+  -webkit-line-clamp: 1; /* 텍스트를 자를 때 원하는 단위 ex) 3줄 */
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`;
+
+const Content = styled.p`
+  display: -webkit-box;
+
+  white-space: normal;
+  -webkit-line-clamp: 2; /* 텍스트를 자를 때 원하는 단위 ex) 3줄 */
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 `;
