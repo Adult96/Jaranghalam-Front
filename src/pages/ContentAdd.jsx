18410c937668d8d74ff4449c3f3b748fd60be91f
junhyuck park <wnsgur1855@naver.com>
2023-02-24 23:48:26 +0900

    feat: img파일 추가 기능

--- src/pages/ContentAdd.jsx
+++ src/pages/ContentAdd.jsx
@@ -7,6 +7,7 @@ import ThemeMode from '../components/ThemeMode';
 import { Btn } from '../elements/Button';
 import { useDispatch } from 'react-redux';
 import { __postContent } from '../utils/redux/modules/board/postAdd';
+import axios from 'axios';
 
 const StContentAddBox = styled.div`
   display: flex;
@@ -19,26 +20,44 @@ const StContentAddBox = styled.div`
 
 const StInputForm = styled.form`
   display: flex;
+  flex-direction: column;
+  gap: 10px;
   flex-wrap: wrap;
 `;
 
 const StInputBox = styled.input`
   display: block;
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.bgBtnColor};
 `;
 
 function ContentAdd() {
   const [darkMode, setDarkMode] = useState(true);
   const theme = darkMode ? darkTheme : lightTheme;
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('hi');
-  const [img, setImg] = useState('');
+  const [title, setTitle] = useState();
+  const [content, setContent] = useState();
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const dispatch = useDispatch();
 
-  const onSubmitAddButtonHandler = (a, b) => {
-    const result = dispatch(__postContent({ title, content }));
-    alert('d');
-    console.log('result', result);
+  const formHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const onSubmitAddButtonHandler = () => {
+    const formData = new FormData();
+    formData.append('image', selectedFile);
+    // 서버에 이미지 업로드 요청 보내기
+    axios.post('http://localhost:4001/board', formData).then((response) => {
+      // 업로드된 파일의 URL을 사용하여 게시물 데이터를 만듦
+      const post = {
+        title,
+        content,
+        imageUrl: response.data.url,
+      };
+      // 생성된 게시물 데이터를 서버에 전송
+      dispatch(__postContent(post));
+    });
   };
 
   return (
@@ -46,13 +65,14 @@ function ContentAdd() {
       <StContentAddBox>
         <GlobalStyle />
         <ThemeMode darkMode={darkMode} onDarkMode={setDarkMode} />
-        <StInputForm>
+        <StInputForm onSubmit={formHandler} enctype="multipart/form-data">
           <StInputBox
             type="text"
             value={title}
             onChange={(e) => {
               setTitle(e.target.value);
             }}
+            placeholder="title"
           />
           <StInputBox
             type="text"
@@ -60,10 +80,14 @@ function ContentAdd() {
             onChange={(e) => {
               setContent(e.target.value);
             }}
+            placeholder="content"
           />
+          <StInputBox type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
+
           <Btn type="button" onClick={() => onSubmitAddButtonHandler(title, content)}>
-            꾺꾺
+            꾺꾺(모든것)
           </Btn>
+          <img src={selectedFile && URL.createObjectURL(selectedFile)} alt="selected image" />
         </StInputForm>
       </StContentAddBox>
     </ThemeProvider>
