f308925d85464b6f1d964c07b473747982d5ae8e
junhyuck park <wnsgur1855@naver.com>
2023-02-26 06:58:01 +0900

    feat: 모달 기능구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import ContentAdd from './ContentAdd';
+import { StModalBackground, StModalContainer } from './ContentAdd';
+
+// const TestBg = styled.div`
+//   background: url('https://i.imgur.com/SvyQWWU.jpg');
+//   background-size: cover;
+//   background-repeat: no-repeat;
+//   width: 100vw;
+//   height: 100vh;
+//   overflow: hidden;
+// `;
 
 export default function Home() {
-  return <div>home</div>;
+  const [showModal, setShowModal] = useState(false);
+
+  const ModalHandler = () => {
+    setShowModal((prev) => !prev);
+  };
+
+  return (
+    <>
+      <button onClick={ModalHandler}> 모달버튼</button>
+      {showModal ? <ContentAdd toggleModal={ModalHandler} /> : null}
+    </>
+  );
 }
