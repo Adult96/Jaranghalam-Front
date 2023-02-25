96f11cce54d1ca9baacab1f1b12a1148a639c7b1
junhyuck park <wnsgur1855@naver.com>
2023-02-26 06:57:49 +0900

    feat: 모달창 구현

--- src/pages/ContentAdd.jsx
+++ src/pages/ContentAdd.jsx
@@ -1,99 +1,134 @@
 import React from 'react';
 import { useState } from 'react';
-import GlobalStyle from '../styles/globalStyle';
-import styled, { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from '../styles/theme';
-import ThemeMode from '../components/ThemeMode';
-import { Btn } from '../elements/Button';
+import styled from 'styled-components';
+import Button from '../elements/Button';
+import Input from '../elements/input';
 import { useDispatch } from 'react-redux';
 import { __postContent } from '../utils/redux/modules/board/postAdd';
 import axios from 'axios';
 
-const StContentAddBox = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+const StH3 = styled.h3`
+  margin-bottom: 50px;
+  color: red;
+  border-bottom: 2px solid ${(props) => props.theme.text};
 `;
 
 const StInputForm = styled.form`
   display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-around;
+  margin-left: 60px;
+  margin-top: 50px;
+  gap: 30px;
+`;
+
+const StTitleContentInputBox = styled.div`
+  overflow: auto;
+  border-radius: ${(props) => props.theme.borderRadius.medium};
+  padding-top: 20px;
+`;
+
+const StTextAreaBox = styled.textarea`
+  margin-top: 20px;
+`;
+
+const StFileInputBox = styled.div`
+  padding-top: 20px;
+  overflow: auto;
+`;
+
+//모달 css
+export const StModalBackground = styled.div`
+  display: flex;
+  position: fixed;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background: #ebe1e18a;
+  backdrop-filter: blur(3px);
+`;
+
+export const StModalContainer = styled.div`
+  display: flex !important;
   flex-direction: column;
-  gap: 10px;
-  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 400px;
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: white;
 `;
 
-const StInputBox = styled.input`
-  display: block;
-  color: ${(props) => props.theme.text};
-  background-color: ${(props) => props.theme.bgBtnColor};
-`;
-
-function ContentAdd() {
-  const [darkMode, setDarkMode] = useState(true);
-  const theme = darkMode ? darkTheme : lightTheme;
-  const [title, setTitle] = useState();
-  const [content, setContent] = useState();
+function ContentAdd({ toggleModal }) {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   const dispatch = useDispatch();
 
-  const form = (e) => {
-    e.preventDefault();
-  };
-
-  const onSubmitAddButtonHandler = () => {
+  //모달 속 추가 핸들러
+  const onConfirmButtonHandler = () => {
     const formData = new FormData();
     formData.append('image', selectedFile);
-    // 서버에 이미지 업로드 요청 보내기
     axios.post('http://localhost:4001/board', formData).then((response) => {
-      // 업로드된 파일의 URL을 사용하여 게시물 데이터를 만듦
       const post = {
         title,
         content,
         imageUrl: response.data.url,
       };
-      // 생성된 게시물 데이터를 서버에 전송
       dispatch(__postContent(post));
       setTitle('');
       setContent('');
+      toggleModal();
     });
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <StContentAddBox>
-        <GlobalStyle />
-        <ThemeMode darkMode={darkMode} onDarkMode={setDarkMode} />
-        <StInputForm onSubmit={form} enctype="multipart/form-data">
-          <StInputBox
-            type="text"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-            placeholder="title"
-          />
-          <StInputBox
-            type="text"
-            value={content}
-            onChange={(e) => {
-              setContent(e.target.value);
-            }}
-            placeholder="content"
-          />
-          <StInputBox type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
-
-          <Btn type="button" onClick={() => onSubmitAddButtonHandler(title, content)}>
-            꾺꾺(모든것)
-          </Btn>
-          <img src={selectedFile && URL.createObjectURL(selectedFile)} alt="selected image" />
-        </StInputForm>
-      </StContentAddBox>
-    </ThemeProvider>
+    <StModalBackground>
+      <StModalContainer>
+        <StH3>새 게시물 만들기</StH3>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          enctype="multipart/form-data"
+        >
+          <div>
+            <Input
+              type="text"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+              placeholder="title"
+            />
+            <textarea
+              type="text"
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+              placeholder="content"
+            />
+            <p style={{ marginLeft: '150px' }}>
+              <Button test onClick={onConfirmButtonHandler}>
+                꾺
+              </Button>
+            </p>
+          </div>
+          <div>
+            <Input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
+          </div>
+        </form>
+      </StModalContainer>
+    </StModalBackground>
   );
 }
 
 export default ContentAdd;
+
+//  <img src={selectedFile && URL.createObjectURL(selectedFile)} alt="selected image" />
