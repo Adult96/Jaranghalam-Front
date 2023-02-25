0455916362825452452c5fe8b594e8e86242bdcf
Adult96 <djxor007@gmail.com>
2023-02-25 18:42:47 +0900

    design: home에 scrollstyle 설정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,6 +29,27 @@ const HomeWrapper = styled.main`
   height: 100%;
   overflow-x: hidden;
   overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    border-radius: 6px;
+    background: transparent;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: linear-gradient(
+      0deg,
+      rgba(88, 81, 216, 1) 0%,
+      rgba(131, 58, 180, 1) 10%,
+      rgba(193, 53, 132, 1) 20%,
+      rgba(225, 48, 108, 1) 30%,
+      rgba(253, 36, 76, 1) 40%,
+      #f56040 50%,
+      rgba(247, 119, 55, 1) 60%,
+      #fcaf45 70%,
+      #ffdc80 80%
+    );
+    border-radius: 6px;
+  }
 
   @media (max-width: ${props => props.theme.screen.tablet_v}) {
     border-top: 1px solid ${props => props.theme.bgBorderColor};
