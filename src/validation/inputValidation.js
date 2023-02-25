7c959a1069b632cf36ba639c0ab0c51c5e8c589e
Adult96 <djxor007@gmail.com>
2023-02-25 18:44:37 +0900

    feat: validation 기능 추가

---
+++ src/validation/inputValidation.js
@@ -0,0 +1,24 @@
+const Valid = {
+  title(text) {
+    if (text.length <= 0) return false;
+    return true;
+  },
+
+  content(text) {
+    if (text.length <= 0) return false;
+    return true;
+  },
+
+  formEmpty(...text) {
+    const textArr = text.filter(v => v === '');
+    if (textArr.length !== 0) return false;
+    return true;
+  },
+
+  pwDifferentCheck(...text) {
+    if (new Set([...text]).size === text.length) return false;
+    return true;
+  },
+};
+
+export default Valid;
